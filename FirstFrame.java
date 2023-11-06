bcdd6ee38aea5c51c07edf13b120cdfa4d46db8d
Simone Marconi <simo.marconi03@gmail.com>
2023-11-06 21:00:46 +0100

    slider aggiunto

--- FirstFrame.java
+++ FirstFrame.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.util.Hashtable;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -5,20 +6,30 @@ import javax.swing.JSlider;
 
 public class FirstFrame extends JFrame{
 
+
     final int maxs = 300;
     final int mins = 5;
     final int base = 10;
 
     public FirstFrame(){
-        this.setSize(600, 300);
-        JSlider slider = new JSlider(JSlider.VERTICAL, mins, maxs, base);
-        this.add(slider);
 
         Hashtable<Integer, JLabel> table = new Hashtable<Integer, JLabel>();
-        table.put(0, new JLabel("5 sec"));
+        table.put(5, new JLabel("5 sec"));
         table.put(maxs/2, new JLabel("2.30 min"));
         table.put(maxs, new JLabel("5 min"));
 
+        this.setSize(600, 300);
+
+        //slider handler
+        JSlider slider = new JSlider(JSlider.VERTICAL, mins, maxs, base);
+        slider.setPaintTrack(true);
+        slider.setPaintTicks(true);
+        slider.setMajorTickSpacing(50);
+        slider.setMinorTickSpacing(10);
+        slider.setSnapToTicks(true);
+        slider.setLabelTable(table);
+        slider.setPaintLabels(true);
+        this.add(slider, BorderLayout.LINE_START);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
